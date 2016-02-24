732baa0673cfe091622731ba3d904303332c4970
Alex <alexandre.straubhaar@he-arc.ch>
2016-02-24 23:35:10 +0100

    Kek

--- gen.py
+++
@@ -1,7 +0,0 @@
-__author__ = "Alex"
-
-class gen:
-    def __init__(self):
-
-    def loop():
-        
--- tp_gen.py
+++ tp_gen.py
@@ -1,9 +1,10 @@
 __author__ = "Alex"
 import sys
 import math
-import gen
+import random
 
-class city:
+#Classes
+class City:
     def __init__(self, name, x, y):
         self.name = name;
         self.x = x;
@@ -12,22 +13,51 @@ class city:
     def __str__(self):
         return (self.name + " " + self.x + " " + self.y)
 
-"""Récupération des villes dans les fichiers et stockage dans un tableau d'objets 'city'"""
+    def position(self):
+        return self.x, self.y
+
+    def distance(self, dest):
+        return math.hypot(self.x - dest.x, self.y - dest.y)
+
+
+class Chromosome:
+    def __init__(self, genes, distances):
+        self.genes = genes
+        self.eval = evaluate(self, distances)
+        
+
+#Fonctions
+def evaluate(population, distances):
+    evaluation = 0
+    for i in range(len(population.genes) - 1):
+        evaluation += distances[population.genes[i]][population.genes[i + 1]]
+    return evaluation
+
+#Récupération des villes dans les fichiers et stockage dans un tableau d'objets 'city'
 def cityParse(file):
     cities = []
     for line in file:
         data = []
         for info in line.split(' '):
             data.append(info)
-        cities.append(city(data[0], data[1], data[2]))
+        cities.append(City(data[0], data[1], data[2]))
     return cities
 
-def distance(city1, city2):
-    return math.sqrt(math.pow(int(city2.x) - int(city1.x), 2) + math.pow(int(city2.y) - int(city1.y), 2))
-
+# Sélection par roue
+def roue(population, taille):
+    moyenne = 0
+    for p in population:
+        moyenne += p.eval
+    moyenne /= taille
+    for p in population:
+        p.chance = moyenne / p.eval
+    nouvellepop = []
+    for i in xrange(taille / 2):
+        nouvellepop[len(nouvellepop):] = [population.population(random.randint(0, len(population) - 1))]
+    population.extend(nouvellepop)
+    return nouvellepop
 
 
 if __name__ == "__main__":
     file = open(sys.argv[1], 'r')
     results = cityParse(file)
-    print(distance(results[0], results[1]))
