b53077436275015807e19b2f0f1fb2e7bfcae551
Alex <alexandre.straubhaar@he-arc.ch>
2016-03-01 23:03:37 +0100

    last version

--- StraubhaarKunzi.py
+++ StraubhaarKunzi.py
@@ -24,7 +24,7 @@ class Chromosome:
     def __init__(self, genes, distances):
         self.genes = genes
         self.eval = evaluate(self, distances)
-        
+
 
 #Fonctions
 def evaluate(population, distances):
@@ -32,6 +32,31 @@ def evaluate(population, distances):
     for i in range(len(population.genes) - 1):
         evaluation += distances[population.genes[i]][population.genes[i + 1]]
     return evaluation
+
+def croisement(pere, mere):
+    nouveau = father[:random.randint(0, len(pere) - 1)]
+    nouveau[len(nouveau):] = [j for j in mere if j not in nouveau]
+    return nouveau
+
+def evolution(chromosomes, distances):
+    taille = len(chromosomes)
+
+    #Sélection par roue
+    nouvellepop = roue(chromosomes, taille)
+
+    #Croisement
+    for i in xrange(0, len(nouvellepop), 2):
+        pere = nouvellepop[i].genes
+        mere = nouvellepop[i + 1].genes
+
+        nouvellepop[len(nouvellepop):] = [Chromosome(croisement(pere, mere), distances)]
+        nouvellepop[len(nouvellepop):] = [Chromosome(croisement(mere, pere), distances)]
+
+    #Mutation
+    for i in xrange(len(nouvellepop)):
+        if random.randint(0, 10) < 1:
+            coupe1 = random.randint(1, len(nouvellepop[i].genes) - 2)
+            coupe2 = random.randint(coupe1 + 1, len(nouvellepop[i].genes) - 1)
 
 #Récupération des villes dans les fichiers et stockage dans un tableau d'objets 'city'
 def cityParse(file):
